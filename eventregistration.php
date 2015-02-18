cd31dac8f6bf84af0731e165c42c24c5ca9db7a6
ginkgoMZD <mzd@ginkgostreet.com>
2015-02-18 16:58:50 -0500

    ext boilerplate and implementation of hook conditionalFields

---
+++ eventregistration.php
@@ -0,0 +1,115 @@
+<?php
+
+require_once 'eventregistration.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function eventregistration_civicrm_config(&$config) {
+  _eventregistration_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function eventregistration_civicrm_xmlMenu(&$files) {
+  _eventregistration_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function eventregistration_civicrm_install() {
+  _eventregistration_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function eventregistration_civicrm_uninstall() {
+  _eventregistration_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function eventregistration_civicrm_enable() {
+  _eventregistration_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function eventregistration_civicrm_disable() {
+  _eventregistration_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function eventregistration_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _eventregistration_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function eventregistration_civicrm_managed(&$entities) {
+  _eventregistration_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function eventregistration_civicrm_caseTypes(&$caseTypes) {
+  _eventregistration_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function eventregistration_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _eventregistration_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+function eventregistration_civicrm_conditionalFields(array &$formList) {
+  $formList['CRM_Event_Form_Registration_Register'] = array(
+    'extension' => 'org.ncba.eventregistration',
+    'entityID' => '1',
+  );
+}
