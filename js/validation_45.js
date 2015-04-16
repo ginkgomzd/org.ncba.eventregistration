4c1af834796fa42ea8c314d4e7131d41b9cc2eb8
ginkgoMZD <mzd@ginkgostreet.com>
2015-04-16 17:15:14 -0400

    configure Annual Conference

---
+++ js/validation_45.js
@@ -0,0 +1,54 @@
+cj(function($) {
+
+  var priceFields =  {
+    EARLYREG: 'price_53',
+    FULLREG: 'price_54',
+    MON: 'price_44_186',
+    TUES: 'price_45_187',
+    WED: 'price_46_188',
+    RECEPTION: 'price_47_189',
+  };
+
+ var btnSubmit = $('#crm-submit-buttons .form-submit');
+
+  btnSubmit.click(function(ev) {
+    ev.preventDefault();
+
+    if (verifyMinimumSelection()) {
+console.log('verified');
+      $('form#Register').submit();
+    } else {
+console.log('warn');
+      warnMinimumSelection();
+    }
+  });
+  
+
+function verifyMinimumSelection() {
+  var anySelection = false;
+  $.each(priceFields, function(index, item) {
+    $el = $('#'+item);
+   if (
+      $el.length > 0  
+      && (
+      $el.is('select') && $el.val() !== ''
+      || ($el.is('[type=radio]') || $el.is('[type=checkbox]'))
+        && $el.is(':checked')
+      )
+    ) {
+      anySelection = true;
+    }
+  });
+  return anySelection;
+} 
+
+function warnMinimumSelection() {
+  CRM.confirm({}, {
+      title: 'Error',
+      message: 'You must select either a full registration or a single-day registration.'},
+     'ok');
+}
+
+});
+
+
