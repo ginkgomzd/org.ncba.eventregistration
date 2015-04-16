e929371f14d965385bf93c11ee88037228568dff
ginkgoMZD <mzd@ginkgostreet.com>
2015-04-16 16:57:39 -0400

    correct logic; update field selectors;

--- js/validation_43.js
+++ js/validation_43.js
@@ -3,27 +3,21 @@ cj(function($) {
   var priceFields =  {
     EARLYREG: 'price_56',
     FULLREG: 'price_57',
-    THURS: 'price_41\\[183\\]',
-    FRI: 'price_42\\[184\\]',
-    SAT: 'price_43\\[185\\]',
+    THURS: 'price_41_183',
+    FRI: 'price_42_184',
+    SAT: 'price_43_185',
   };
 
-  //testing:
-  priceFields = {
-    two: 'CIVICRM_QFID_3_4',
-    one: 'CIVICRM_QFID_2_2',
-    three: 'CIVICRM_QFID_4_6',
-    select: 'price_3',
-  }
-
-  var btnSubmit = $('#crm-submit-buttons .form-submit');
+ var btnSubmit = $('#crm-submit-buttons .form-submit');
 
   btnSubmit.click(function(ev) {
     ev.preventDefault();
 
     if (verifyMinimumSelection()) {
-      $('form').submit();
+console.log('verified');
+      $('form#Register').submit();
     } else {
+console.log('warn');
       warnMinimumSelection();
     }
   });
@@ -33,12 +27,14 @@ function verifyMinimumSelection() {
   var anySelection = false;
   $.each(priceFields, function(index, item) {
     $el = $('#'+item);
-    if (
-      $el.length > 0 && (
-        $el.is(':selected') ||
-        $el.is(':checked')  ||
-        $el.val() !== ''
-    )) {
+   if (
+      $el.length > 0  
+      && (
+      $el.is('select') && $el.val() !== ''
+      || ($el.is('[type=radio]') || $el.is('[type=checkbox]'))
+        && $el.is(':checked')
+      )
+    ) {
       anySelection = true;
     }
   });
@@ -51,7 +47,6 @@ function warnMinimumSelection() {
       message: 'You must select either a full registration or a single-day registration.'},
      'ok');
 }
-
 
 });
 
